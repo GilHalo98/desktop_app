e68b8e8876b064d9ee17c4fd91203794c105c395
GilHalo98 <diego_gilhalo98@hotmail.com>
2023-11-05 15:36:10 -0600

    Campos de la barra de busqueda agregados

---
+++ renderer/pages/listaLineas/componentes/inputBusqueda.tsx
@@ -0,0 +1,25 @@
+import {
+    Container, Row, Col,
+    Input, Label
+} from 'reactstrap';
+
+export default function InputBusqueda(
+    props: any
+) {
+    return(
+        <Container>
+            <Row>
+                <Col>
+                    <Input
+                        id="nombreLinea"
+                        placeholder="Nombre de la linea"
+                        type="text"
+                        onChange={(input) => {
+                            props.setNombreLinea(input.target.value);
+                        }}
+                    />
+                </Col>
+            </Row>
+        </Container>
+    );
+};
