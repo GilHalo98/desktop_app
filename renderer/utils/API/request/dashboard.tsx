e5c5c473f63c3242df42fb3da943f15a328854e7
Diego Gil <halo98@pop-os.localdomain>
2023-11-10 00:15:33 -0600

    Request del endpoint dashboard agregado

---
+++ renderer/utils/API/request/dashboard.tsx
@@ -0,0 +1,36 @@
+// Importa la libreria para las consultas.
+import axios from "axios";
+
+// Importamos los endpoints
+import { ENDPOINTS } from "../endpoints";
+
+const GetSeguimientoPiezas = async (
+    parametros: {
+        limit: number,
+        offset: number,
+        idLineaVinculada: number
+    }
+) => {
+    return axios({
+        method: 'get',
+        url: ENDPOINTS.DASHBOARD.SEGUIMIENTO.PIEZAS,
+        params: parametros
+    });
+};
+
+const GetBuscarPieza = async (
+    parametros: {
+        dataMatrix: string
+    }
+) => {
+    return axios({
+        method: 'get',
+        url: ENDPOINTS.DASHBOARD.BUSCAR.PIEZA,
+        params: parametros
+    });
+};
+
+export {
+    GetSeguimientoPiezas,
+    GetBuscarPieza
+};
